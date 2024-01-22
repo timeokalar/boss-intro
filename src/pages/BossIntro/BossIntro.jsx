a8183a7b06dfcc9ddbb80be32f505ae72dc2e770
Jared Kuntz <timeo.kalar.ultros@gmail.com>
2024-01-22 11:43:22 -0600

    Add share button

--- src/pages/BossIntro/BossIntro.jsx
+++ src/pages/BossIntro/BossIntro.jsx
@@ -19,22 +19,28 @@ function BossIntro() {
   const firstName = searchParams.get("name1");
   const secondName = searchParams.get("name2");
   const bossImageFromParam = searchParams.get("img");
-  const blurb = `${firstLine.toUpperCase()}
-  ${secondLine.toUpperCase()}
-  ${thirdLine.toUpperCase()}
-  ${fourthLine.toUpperCase()}`;
+  const blurb = `${firstLine?.toUpperCase() || "First Line"}
+  ${secondLine?.toUpperCase() || "Second Line"}
+  ${thirdLine?.toUpperCase() || "Third Line"}
+  ${fourthLine?.toUpperCase() || "Fourth Line"}`;
   return (
     <>
       <div className="flash"></div>
       <div className="grid crt">
         <div className="background-panel-top">
-          <p className="name-text">{firstName.toUpperCase()}</p>
-          <p className="crt-text black-text">{firstName.toUpperCase()}</p>
+          <p className="name-text">{firstName?.toUpperCase() || "Top Text"}</p>
+          <p className="crt-text black-text">
+            {firstName?.toUpperCase() || "Top Text"}
+          </p>
         </div>
         <div className="middle-panel"></div>
         <div className="background-panel-bottom ">
-          <p className="name-text ">{secondName.toUpperCase()}</p>
-          <p className="crt-text black-text ">{secondName.toUpperCase()}</p>
+          <p className="name-text ">
+            {secondName?.toUpperCase() || "Bottom Text"}
+          </p>
+          <p className="crt-text black-text ">
+            {secondName?.toUpperCase() || "Bottom Text"}
+          </p>
         </div>
       </div>
       <iframe
@@ -54,6 +60,15 @@ function BossIntro() {
         alt="boss"
       />
       <p className="blurb">{blurb}</p>
+      <button
+        className="share-button"
+        onClick={() => {
+          window.navigator.clipboard.writeText(window.location.href);
+          window.alert("A sharable link hass been copied to your clipboard!");
+        }}
+      >
+        Share
+      </button>
     </>
   );
 }
